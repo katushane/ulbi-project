bbdcd203df18ca4c64e7840b48db385fbedee4eb
e.bukina <e.bukina@vk.team>
2023-10-03 23:02:12 +0500

    app router, navbar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,7 @@
-import React, {Suspense} from 'react';
+import React from 'react';
 import './styles/index.scss';
-import {Link, Route, Routes} from "react-router-dom";
 import {useTheme} from "app/providers/ThemeProvider/lib/useTheme";
 import {classNames} from "shared/lib/classnames/classnames";
-import {AboutPage} from "pages/AboutPage";
-import {MainPage} from "pages/MainPage";
 import {AppRouter} from "app/providers/router";
 import {Navbar} from "widgets/Navbar";
 
