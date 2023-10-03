81891ac185bd466e0fe9c0ab3aeb8d477e606d70
e.bukina <e.bukina@vk.team>
2023-10-03 23:01:36 +0500

    app router, navbar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,23 +5,17 @@ import {useTheme} from "app/providers/ThemeProvider/lib/useTheme";
 import {classNames} from "shared/lib/classnames/classnames";
 import {AboutPage} from "pages/AboutPage";
 import {MainPage} from "pages/MainPage";
+import {AppRouter} from "app/providers/router";
+import {Navbar} from "widgets/Navbar";
 
 export const App = () => {
     const {theme, toggleTheme} = useTheme();
 
     return (
         <div className={classNames('app', {hovered: true, some: true}, [theme])}>
+            <Navbar />
+            <AppRouter />
             <button onClick={toggleTheme}>TOGGLE</button>
-            <Link to='/' >Главная</Link>
-            <Link to='/about'>О сайте</Link>
-                <Suspense fallback={<div>Loading...</div>}>
-                    <Routes>
-                        <Route path='/about' element={<AboutPage />} />
-                        <Route path='/' element={<MainPage />} />
-                    </Routes>
-                </Suspense>
-
-            {/*<Counter />*/}
         </div>
     );
 };
